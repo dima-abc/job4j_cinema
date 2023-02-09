ba394877316b88c460c6fc9c32e6717b4748f3b8
dmitryjob4j <haoos@inbox.ru>
2023-02-09 17:57:58 +0300

    create project

---
+++ src/main/java/ru/my/cinema/CinemaApp.java
@@ -0,0 +1,27 @@
+package ru.my.cinema;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 3. Мидл
+ * 3.2. Web
+ * 3.2.9. Контрольные вопросы
+ * 2. Сервис - Кинотеатр [#504869 #293473]
+ * Запуск приложения.
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 09.02.2023
+ */
+@SpringBootApplication
+public class CinemaApp {
+    private static final Logger LOG = LoggerFactory.getLogger(CinemaApp.class.getSimpleName());
+    private static final String START_PAGE = "http://localhost:8080/index";
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaApp.class, args);
+        LOG.info("Go to: {}", START_PAGE);
+    }
+}
---
+++ src/test/java/ru/my/cinema/CinemaAppTest.java
@@ -0,0 +1,22 @@
+package ru.my.cinema;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * 3. Мидл
+ * 3.2. Web
+ * 3.2.9. Контрольные вопросы
+ * 2. Сервис - Кинотеатр [#504869 #293473]
+ *
+ * @author Dmitry Stepanov, user Dmitry
+ * @since 09.02.2023
+ */
+public class CinemaAppTest {
+    @Test
+    public void testTest() {
+        assertThat(true).isTrue();
+    }
+}
